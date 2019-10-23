d730e940da83e3674ea3372afaa99d4ffe2884a2
Olivier Verrier <olivier.verrier@ynov.com>
2019-10-23 14:51:19 +0200

    Exo 1 fini
    Got OK

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,24 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+      let finishingPlace = input[0]
+      for (let index = 1; index < input.length; index++) {
+        let splitedElement = input[index].split(" ")
+        let placesLost = parseInt(splitedElement[0])
+        let placesWon = parseInt(splitedElement[1])
+        finishingPlace += placesLost - placesWon
+      }
+      let winnings = 0
+      if (finishingPlace < 101){
+        winnings = 1000
+      } else if (finishingPlace < 10001){
+        winnings = 100
+      } else {
+        return "KO"
+      }
+      return winnings
 
+    
     // AND HERE
   },
   verify: function (dataset, output) {
