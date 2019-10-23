e76fefc8fa7e7a184d67bec1978634fd37f5c91b
Olivier Verrier <olivier.verrier@ynov.com>
2019-10-23 15:55:53 +0200

    Tp3 pas fini :'(
    Problème de undefined, ma boucle sur le position.x va trop loin et j'arrive pas à régler le soucis sur ma condition

--- tp1/3/index.js
+++ tp1/3/index.js
@@ -7,7 +7,106 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-    
+      const sizeOfMatrix = input.shift()
+      console.log("sizeOfMatrix = ", sizeOfMatrix)
+      // Je récupère ton code pour avoir la matrice, j'espère que c'est autorisé :D
+      console.log("input = ", input)
+      const inputMatrix = input.map(row => row.split(''))
+      console.log("inputMatrix = ", inputMatrix)
+      console.log("inputMatrix[0] = ", inputMatrix[0])
+      console.log("inputMatrix[0][0] = ", inputMatrix[0][0])
+      let isThereStillGoldToPickUp = true
+      let isTherStillMultipliersToPickUp = true
+      
+      let moveInput = ""
+      let position = {
+        x: 0,
+        y: 0
+      }
+      console.log("position = ", position)
+      console.log("position.x = ", position.x)
+      console.log("position.y = ", position.y)
+      // Tant qu'il y a des pièces d'or à récupérer on descend tout en bas de la carte en allant à droite puis à gauche et ainsi de suite
+      while (isThereStillGoldToPickUp){
+        while(position.x < sizeOfMatrix - 1){
+          while (position.y < sizeOfMatrix - 1){
+            console.log("position = ", position)
+            console.log("inputMatrix[position.x][position.y] = ", inputMatrix[position.x][position.y])
+            switch (inputMatrix[position.x][position.y]){
+              case "o":
+                moveInput += "x>"
+                inputMatrix[position.x][position.y] = "."
+                break
+              case "*":
+                moveInput += ">"
+                break
+              default:
+                moveInput += ">"
+            }
+            position.y++
+          }
+          moveInput += "v"
+          position.x++
+          while (position.y > 0){
+            console.log("position = ", position)
+            console.log("inputMatrix[position.x][position.y] = ", inputMatrix[position.x][position.y])
+            switch (inputMatrix[position.x][position.y]){
+              case "o":
+                moveInput += "x<"
+                break
+              case "*":
+                moveInput += "<"
+                break
+              default:
+                moveInput += "<"
+            }
+            position.y--
+          }
+          moveInput += "v"
+          position.x++
+          console.log(" =")
+        }
+        isThereStillGoldToPickUp = false
+      }
+
+      // Tant qu'il y a des multiplicateurs à récupérer on monte tout en haut de la carte en allant à droite puis à gauche et ainsi de suite
+      while (isTherStillMultipliersToPickUp){
+        while(position.x > 0){
+          while (position.y < sizeOfMatrix - 1){
+            console.log("position = ", position)
+            console.log("inputMatrix[position.x][position.y] = ", inputMatrix[position.x][position.y])
+            switch (inputMatrix[position.x][position.y]){
+              case "*":
+                moveInput += "x>"
+                inputMatrix[position.x][position.y] = "."
+                break
+              default:
+                moveInput += ">"
+            }
+            position.y++
+          }
+          moveInput += "^"
+          position.x--
+          while (position.y > 0){
+            console.log("position = ", position)
+            console.log("inputMatrix[position.x][position.y] = ", inputMatrix[position.x][position.y])
+            switch (inputMatrix[position.x][position.y]){
+              case "*":
+                moveInput += "x<"
+                inputMatrix[position.x][position.y] = "."
+                break
+              default:
+                moveInput += "<"
+            }
+            position.y--
+          }
+          moveInput += "^"
+          position.x--
+        }
+        isTherStillMultipliersToPickUp = false
+      }
+
+      return moveInput
     // AND HERE
   },
   verify: function (dataset, output) {
