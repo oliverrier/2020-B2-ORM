8626b82d547014d52d4119b199d7763d4934a65a
Olivier Verrier <olivier.verrier@ynov.com>
2019-10-23 15:01:25 +0200

    Exo 2 fini
    Got OK

--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,21 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-
+    numberOfBoxes = input[0]
+    console.log("numberOfBoxes = ", numberOfBoxes)
+    weight = 0
+    numberOfAR = 1
+      for (let index = 1; index < numberOfBoxes + 1; index++) {
+        let boxWeight = input[index];
+        weight += boxWeight
+        if (weight > 100) {
+          weight -= boxWeight
+          index --
+          numberOfAR ++
+          weight = 0
+        }
+      }
+      return numberOfAR;
     // AND HERE
   },
   verify: function (dataset, output) {
